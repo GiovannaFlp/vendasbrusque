840c8e80119db6018a90c34028c54e8040aa7e48
GiovannaFlp <giovannaflp@github.com>
2026-09-25 14:55:30 -0300

    fix: reset scroll to top on page navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import { Providers } from './providers'
 import { Header } from '@/components/Header'
 import { Footer } from '@/components/Footer'
+import { ScrollToTop } from '@/components/ScrollToTop'
 
 export const dynamic = 'force-dynamic'
 
@@ -31,6 +32,7 @@ export default function RootLayout({
     <html lang="pt-BR">
       <body>
         <Providers>
+          <ScrollToTop />
           <div className="min-h-screen flex flex-col">
             <Header />
             <main className="flex-1">{children}</main>
